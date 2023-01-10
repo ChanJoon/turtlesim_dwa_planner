4f6edf31cd415c2fe8c85b2e8c3f68cba94f7084
as06047 <as06047@yonsei.ac.kr>
2023-01-11 02:50:22 +0900

    Refactoring...

--- script/main.py
+++ script/main.py
@@ -13,11 +13,11 @@ class Config:
     self.min_speed = -0.5  # [m/s]
     self.max_yaw_rate = 90.0 * math.pi / 180.0  # [rad/s]
     self.max_accel = 5.0  # [m/ss]
-    self.max_delta_yaw_rate = 50.0 * math.pi / 180.0  # [rad/ss]
-    self.v_resolution = 0.2  # [m/s]
+    self.max_delta_yaw_rate = 90.0 * math.pi / 180.0  # [rad/ss]
+    self.v_resolution = 0.3  # [m/s]
     self.yaw_rate_resolution = 0.5 * math.pi / 180.0  # [rad/s]
     self.dt = 0.1  # [s] Time tick for motion prediction
-    self.predict_time = 1.5  # [s]
+    self.predict_time = 1.0  # [s]
     self.to_goal_cost_gain = 0.15
     self.speed_cost_gain = 1.0
     self.obstacle_cost_gain = 1.0
@@ -36,17 +36,15 @@ class Turtle:
   def __init__(self):
     rospy.init_node('main', anonymous=True)
     self.ob_sub = rospy.Subscriber("turtle1/pose", Pose, self.ob_callback)
-    self.ob_pub = rospy.Publisher("turtle1/cmd_vel", Twist, queue_size=10)
+    self.ob_pub = rospy.Publisher("turtle1/cmd_vel", Twist, queue_size=1)
     self.pose_sub = rospy.Subscriber("turtle2/pose", Pose, self.pose_callback)
-    self.vel_pub = rospy.Publisher("turtle2/cmd_vel", Twist, queue_size=10)
+    self.vel_pub = rospy.Publisher("turtle2/cmd_vel", Twist, queue_size=1)
     self.rate = rospy.Rate(10)
     
     self.config = Config()
     self.x = np.array([0.0, 0.0, math.pi / 8.0, 0.0, 0.0])
-    self.trajectory = np.array(self.x)
     self.turtle2_pose = Pose()
-    self.isDone = False
-    self.i = 0
+    self.goalIndex = 0
   
     
   def ob_callback(self, msg):
@@ -54,50 +52,55 @@ class Turtle:
     
   def pose_callback(self, msg):
     self.turtle2_pose = msg
-    self.init_x = self.turtle2_pose.x
-    self.init_y = self.turtle2_pose.y
-    self.init_theta = self.turtle2_pose.theta
-    self.init_v = self.turtle2_pose.linear_velocity
-    self.init_omega = self.turtle2_pose.angular_velocity
+    self.turtle2_x = self.turtle2_pose.x
+    self.turtle2_y = self.turtle2_pose.y
+    self.turtle2_theta = self.turtle2_pose.theta
+    self.turtle2_v = self.turtle2_pose.linear_velocity
+    self.turtle2_omega = self.turtle2_pose.angular_velocity
   
     self.main()
   
-  def main(self):
+  def dyn_obstacle(self, forward_speed=0.5, yaw_rate=0.5):
+    # dynamic obstacle setting for circular motion
     ob_vel = Twist()
-    ob_vel.linear.x = 0.5
-    ob_vel.angular.z = 0.5
+    ob_vel.linear.x = forward_speed
+    ob_vel.angular.z = yaw_rate
     self.ob_pub.publish(ob_vel)
     
-    if self.i == len(self.config.waypoints):
-      self.isDone = True
-      self.i = 0
-      
-    # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
-    self.x = np.array([self.init_x, self.init_y, self.init_theta, self.init_v, self.init_omega])
-
-    # input [forward speed, yaw_rate]
-    msg = Twist()
-
+  
+  def main(self):
+    
+    # obstacles [x(m) y(m), ....]
+    self.dyn_obstacle(forward_speed=1.0, yaw_rate=0.8)
     ob = self.config.ob
 
     # goal position [x(m), y(m)]
-    goal = self.config.waypoints[self.i]
-    rospy.loginfo("\nCurrent pose x: %.2f, y: %.2f", self.x[0], self.x[1])
-    print("Goal    pose x: {}, y: {}".format(goal[0], goal[1]))
-    u, predicted_trajectory = dwa.dwa_control(self.x, self.config, goal, ob)
-    # x = dwa.motion(x, u, self.config.dt)  # simulate robot
-    self.trajectory = np.vstack((self.trajectory, self.x))  # store state history
-  
+    if self.goalIndex == len(self.config.waypoints):
+      self.goalIndex = 0
+    goal = self.config.waypoints[self.goalIndex]
+      
+    # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
+    self.x = np.array([self.turtle2_x, self.turtle2_y, self.turtle2_theta, self.turtle2_v, self.turtle2_omega])
+
+    # input [forward speed, yaw_rate]
+    u, _ = dwa.dwa_control(self.x, self.config, goal, ob)
+    
+    msg = Twist()
     msg.linear.x = u[0]
     msg.angular.z = u[1]
-    print("\nlinear\n   x: {}\nangular\n   z: {}\n".format(msg.linear.x, msg.linear.z))
+    
+    # publish cmd_vel to turtle2
     self.vel_pub.publish(msg)
+    
+    rospy.loginfo("\nCurrent pose x: %.2f, y: %.2f", self.x[0], self.x[1])
+    print("Goal    pose x: {}, y: {}".format(goal[0], goal[1]))
+    print("\nlinear\n   x: {}\nangular\n   z: {}\n".format(msg.linear.x, msg.angular.z))
 
     # check reaching goal
     dist_to_goal = math.hypot(self.x[0] - goal[0], self.x[1] - goal[1])
     if dist_to_goal <= self.config.robot_radius:
       print("----------[ Goal ]----------")
-      self.i += 1
+      self.goalIndex += 1
 
 if __name__ == '__main__':
   try:
